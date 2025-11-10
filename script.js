b7c1ef1db56acdd2d49f259081186ce3a5f3d104
jerriker <befikerkassahuntessema@gmail.com>
2025-11-11 00:23:58 +0300

    fixed the entire structure

--- script.js
+++ script.js
@@ -1,80 +1,148 @@
 sketch = document.querySelector('.sketch')
-cellSizeDisplay = document.querySelector('.size-display')
 inputSize = document.getElementById('size')
+sizeDisplay = document.querySelector('.size-display')
 
 // Buttons
+colorPalette = document.getElementById('color-palette')
 randomColorBtn = document.getElementById('random-color')
+eraseBtn = document.getElementById('erase')
 gridBtn = document.getElementById('grid')
+clearBtn = document.getElementById('clear')
 
-// Default size
-const defaultSize = 16
+// Color
+const colors = document.querySelectorAll('.color')
 
-cellSizeDisplay.textContent = `${defaultSize}X${defaultSize}`
+// Default Size
+
+let defaultSize = 16
+sizeDisplay.textContent = `${defaultSize}X${defaultSize}`
 inputSize.value = defaultSize
-
 for (let i = 1; i <= defaultSize ** 2; i++) {
     const defaults = document.createElement('div')
     defaults.classList.add('cell')
     defaults.style.width = `${100 / defaultSize}%`
     defaults.style.height = `${100 / defaultSize}%`
-
-    defaults.addEventListener('mouseover', () => {
-        if (userRandomColor)
-            defaults.style.backgroundColor = randomColorGenerate()
-    })
     sketch.appendChild(defaults)
 }
 
+// New Grid
+const createCell = () => {
+    sketch.innerHTML = ''
+    let size = inputSize.value
+    sizeDisplay.textContent = `${size}X${size}`
+    for (let i = 1; i <= size ** 2; i++) {
+        const defaults = document.createElement('div')
+        defaults.classList.add('cell')
+        defaults.style.width = `${100 / size}%`
+        defaults.style.height = `${100 / size}%`
+        defaults.style.border = gridEnabled ? '1px solid #ddd' : 'none'
+        sketch.appendChild(defaults)
+    }
+
+}
+inputSize.addEventListener('input', () => {
+    createCell();
+})
+
+// Add color
+let colorPaletteEnabled = true
+
+// const inputColorPalette = () => {
 
 
-// Random Color
+// }
+
+// choseColor() = 'black'
+const choseColor = () => {
+    colors.forEach(color => {
+        color.addEventListener('mouseover', () => {
+            // console.log(color.id)
+            return color.id
+        })
+    })
+}
+
+
+const getColorPalette = () => {
+    colorPaletteEnabled = !colorPaletteEnabled
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.style.backgroundColor = colorPaletteEnabled ? randomColorGenerate() : choseColor()
+    })
+
+
+}
+colorPalette.addEventListener('click', () => {
+    getColorPalette()
+})
+
+// Add random Color
 const randomColorGenerate = () => {
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
     const b = Math.floor(Math.random() * 256)
-    return `rgb(${r}, ${g}, ${b})`
+    randomColor = `rgb(${r}, ${g}, ${b})`
+    return randomColor
 }
 
-let userRandomColor = false
-randomColorBtn.addEventListener('click', () => {
-    userRandomColor = !userRandomColor
-})
+let randomColorEnabled = false
+let eraseEnabled = false
 
-// Modify Size 
-
-const createCell = () => {
-    cellSizeDisplay.textContent = `${inputSize.value}X${inputSize.value}`
-    sketch.innerHTML = ''
-
-    let size = inputSize.value
-    for (let i = 1; i <= size ** 2; i++) {
-        const newCell = document.createElement('div')
-        newCell.classList.add('cell')
-        newCell.style.width = `${100 / size}%`
-        newCell.style.height = `${100 / size}%`
-
-        newCell.addEventListener('mouseover', () => {
-            if (userRandomColor)
-                newCell.style.backgroundColor = randomColorGenerate()
-        })
-        sketch.appendChild(newCell)
-
-    }
-}
-
-inputSize.addEventListener('input', () => createCell())
-
-// Grid
-let gridEnabled = true;
-
-gridBtn.addEventListener('click', () => {
-    const cells = document.querySelectorAll('.cell');
-    gridEnabled = !gridEnabled;
+const getRandomColor = () => {
+    randomColorEnabled = !randomColorEnabled
+    const cells = document.querySelectorAll('.cell')
 
     cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            if (randomColorEnabled)
+                cell.style.backgroundColor = randomColorGenerate()
+        })
+    })
+    eraseEnabled = false
+    // randomColorBtn.textContent = randomColorEnabled ? 'On' : 'Off'
 
-        cell.style.border = gridEnabled ? '1px solid #ddd' : 'none';
-    });
+}
+randomColorBtn.addEventListener('click', () => {
+    getRandomColor()
+})
 
-    // gridBtn.textContent = gridEnabled ? 'Grid ON' : 'Grid OFF';
-});
+
+// Add Grid
+let gridEnabled = false
+
+const displayGrid = () => {
+    gridEnabled = !gridEnabled
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.style.border = gridEnabled ? '1px solid #ddd' : 'none'
+    })
+}
+
+gridBtn.addEventListener('click', () => displayGrid())
+
+// Clear
+
+const clearGrid = () => {
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => cell.style.backgroundColor = '#fff')
+}
+
+clearBtn.addEventListener('click', () => clearGrid())
+
+
+// Erase
+
+const eraseGrid = () => {
+    eraseEnabled = !eraseEnabled
+    const cells = document.querySelectorAll('.cell')
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            if (eraseEnabled)
+                cell.style.backgroundColor = '#fff'
+        })
+    })
+    randomColorEnabled = false
+    // eraseBtn.textContent = eraseEnabled ? 'On' : 'Off'
+}
+
+eraseBtn.addEventListener('click', () => eraseGrid())
